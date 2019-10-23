ef37bb896c5cc16a602806de87bfb817be243116
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-10-23 16:06:36 +0100

    t Make Qt5 available to Catch2_Tests

--- tests/Catch2_Tests/main.cpp
+++ tests/Catch2_Tests/main.cpp
@@ -1,6 +1,9 @@
 #define APPROVALS_CATCH
 #include "ApprovalTests.hpp"
 
+// Unused for now - just test that Qt is found by builds
+#include <QApplication>
+
 using namespace ApprovalTests;
 
 auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
