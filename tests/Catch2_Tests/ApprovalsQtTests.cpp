2ede3be8d242a1ff048a1b2868ba1363940c48df
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-08 22:46:49 +0000

    r Exract method

--- tests/Catch2_Tests/ApprovalsQtTests.cpp
+++ tests/Catch2_Tests/ApprovalsQtTests.cpp
@@ -3,6 +3,29 @@
 #include <QTableWidget>
 #include "ApprovalTestsQt/ApprovalsQt.h"
 
+namespace
+{
+    void populateTable(QTableWidget& tableWidget)
+    {
+        const int rows = 2;
+        const int columns = 3;
+        tableWidget.setRowCount(rows);
+        tableWidget.setColumnCount(columns);
+        tableWidget.setHorizontalHeaderLabels(QStringList() << "Column 1"
+                                                            << "Column 2"
+                                                            << "Column 3");
+        for (int i = 0; i != rows; ++i)
+        {
+            for (int j = 0; j != columns; ++j)
+            {
+                tableWidget.setItem(i,
+                    j,
+                    new QTableWidgetItem(QString::number((i + 1) * (j + 1))));
+            }
+        }
+    }
+} // namespace
+
 TEST_CASE("It approves a QImage")
 {
     // begin-snippet: verify_qimage
@@ -15,20 +38,6 @@ TEST_CASE("It approves a QImage")
 TEST_CASE("It approves a QTableWidget")
 {
     QTableWidget tableWidget;
-    const int rows = 2;
-    const int columns = 3;
-    tableWidget.setRowCount(rows);
-    tableWidget.setColumnCount(columns);
-    tableWidget.setHorizontalHeaderLabels(QStringList() << "Column 1"
-                                                        << "Column 2"
-                                                        << "Column 3");
-    for (int i = 0; i != rows; ++i)
-    {
-        for (int j = 0; j != columns; ++j)
-        {
-            tableWidget.setItem(
-                i, j, new QTableWidgetItem(QString::number((i + 1) * (j + 1))));
-        }
-    }
+    populateTable(tableWidget);
     ApprovalTestsQt::verifyQTableView(tableWidget);
 }
