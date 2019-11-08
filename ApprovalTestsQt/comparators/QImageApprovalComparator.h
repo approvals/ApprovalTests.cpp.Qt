a96a394c14341b6f2e20d04bdb0af8993e187865
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-08 18:30:30 +0000

    F Add ApprovalTestsQt::QImageApprovalComparator - to try and fix test failures in Travis (Linux) builds

---
+++ ApprovalTestsQt/comparators/QImageApprovalComparator.h
@@ -0,0 +1,17 @@
+#ifndef APPROVALTESTS_CPP_QT_QIMAGEAPPROVALCOMPARATOR_H
+#define APPROVALTESTS_CPP_QT_QIMAGEAPPROVALCOMPARATOR_H
+
+#include "ApprovalTests.hpp"
+
+namespace ApprovalTestsQt {
+class QImageApprovalComparator : public ApprovalTests::ApprovalComparator {
+public:
+    bool contentsAreEquivalent(std::string receivedPath, std::string approvedPath) const override {
+        const QImage receivedImage(QString::fromStdString(receivedPath));
+        const QImage approvedImage(QString::fromStdString(approvedPath));
+        return receivedImage == approvedImage;
+    }
+};
+} // namespace ApprovalTestsQt
+
+#endif //APPROVALTESTS_CPP_QT_QIMAGEAPPROVALCOMPARATOR_H
--- tests/Catch2_Tests/ApprovalsQtTests.cpp
+++ tests/Catch2_Tests/ApprovalsQtTests.cpp
@@ -1,8 +1,15 @@
 #include "Catch.hpp"
 #include <QImage>
 #include "ApprovalTestsQt/ApprovalsQt.h"
+#include "ApprovalTestsQt/comparators/QImageApprovalComparator.h"
 
 TEST_CASE("It approves a QImage") {
+
+    auto disposer =
+            ApprovalTests::FileApprover::registerComparatorForExtension(
+                    ".png",
+                    std::make_shared<ApprovalTestsQt::QImageApprovalComparator>());
+
     // begin-snippet: verify_qimage
     QImage image(10, 20, QImage::Format_RGB32);
     image.fill(Qt::red);
