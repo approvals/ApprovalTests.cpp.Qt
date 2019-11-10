bc74dde94764041b779aa426dd99b1aa08145a73
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-10 22:09:42 +0000

    Revert "B Add <SingleHpp unalterable> to help fix builds using single header"
    This reverts commit 9de3c10c

--- ApprovalTestsQt/integrations/catch/Catch2QtApprovals.h
+++ ApprovalTestsQt/integrations/catch/Catch2QtApprovals.h
@@ -1,7 +1,6 @@
 #ifndef APPROVALTESTS_CPP_QT_CATCH2QTAPPROVALS_H
 #define APPROVALTESTS_CPP_QT_CATCH2QTAPPROVALS_H
 
-// <SingleHpp unalterable>
 #if defined(APPROVALS_CATCH_QT)
 
 #define APPROVALS_CATCH_EXISTING_MAIN
@@ -33,7 +32,5 @@ int main(int argc, char* argv[])
     return result;
 }
 #endif // APPROVALS_CATCH_QT
-
-// </SingleHpp>
 
 #endif //APPROVALTESTS_CPP_QT_CATCH2QTAPPROVALS_H
