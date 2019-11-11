f32aaf552c7e4b9ac76eb5c68ad679f2e21491a9
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-11 22:31:17 +0000

    t Add initial test, showing "breaking in" to the private widget data

---
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets_tests.cpp
@@ -0,0 +1,18 @@
+#include "Catch.hpp"
+
+#include "samplewidgets.h"
+
+#include <QCheckBox>
+
+TEST_CASE( "SampleWidgets checkbox is initially unchecked")
+{
+    SampleWidgets sampleWidgets;
+
+    // The Ui is private, so we have to "cheat" and search for the contained
+    // widgets ourselves.
+    // This makes for potential maintenance pain in future, if the widget
+    // internals are modified
+    auto checkbox = sampleWidgets.findChild<QCheckBox*>();
+    REQUIRE(checkbox != nullptr);
+    CHECK(! checkbox->isChecked());
+}
