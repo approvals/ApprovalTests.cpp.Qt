7ea007dfac07b333c17b742d48681f64e17f0d83
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-11 21:38:31 +0000

    t Add new test project to find ways to test initially private widgets

---
+++ examples/accessing_widgets/main.cpp
@@ -0,0 +1,6 @@
+#define APPROVALS_CATCH_QT
+#include "ApprovalTestsQt/integrations/catch/Catch2QtApprovals.h"
+
+using namespace ApprovalTests;
+
+auto directory = Approvals::useApprovalsSubdirectory("approval_tests");
