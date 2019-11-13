4e6591e687d72af22e6ef1eb69b6ca19f00209f8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-13 11:03:02 +0000

    t Improve comments in SampleWidgets02Fixture

--- examples/accessing_widgets/accessing_widgets_tests/samplewidgets02_tests.cpp
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets02_tests.cpp
@@ -9,6 +9,11 @@ class SampleWidgets02Fixture : public SampleWidgets02
 {
 public:
     // Bump visibility from protected to public, for testing
+    // Pro: The public interface of SampleWidgets02 doesn't contain
+    //      things only required for testing
+    // Con: Future developers may be tempted to inherit SampleWidgets02
+    //      in order to break encapsulatiom and use things enabled only for
+    //      testing
     using SampleWidgets02::goButton;
 };
 
