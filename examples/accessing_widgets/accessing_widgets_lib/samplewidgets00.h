e6371a5c8912401468d3619db3c3a37575741505
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-13 11:21:38 +0000

    t Add samplewidgets00 test (identical to samplewidgets01 for now) to show what code looks like without fixture.

---
+++ examples/accessing_widgets/accessing_widgets_lib/samplewidgets00.h
@@ -0,0 +1,24 @@
+#ifndef APPROVALTESTS_CPP_QT_SAMPLEWIDGETS00_H
+#define APPROVALTESTS_CPP_QT_SAMPLEWIDGETS00_H
+
+#include <QGroupBox>
+
+namespace Ui
+{
+    class SampleWidgets;
+}
+
+//! Ui::SampleWidgets is a private data member - tests will use findChild*()
+class SampleWidgets00 : public QGroupBox
+{
+    Q_OBJECT
+
+public:
+    explicit SampleWidgets00(QWidget* parent = nullptr);
+    ~SampleWidgets00();
+
+private:
+    Ui::SampleWidgets* ui;
+};
+
+#endif //APPROVALTESTS_CPP_QT_SAMPLEWIDGETS00_H
---
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets00_tests.cpp
@@ -0,0 +1,35 @@
+#include "Catch.hpp"
+
+#include "samplewidgets00.h"
+
+#include <QToolButton>
+
+//! Ui::SampleWidgets is a private data member - tests will use findChild*()
+class SampleWidgets00Fixture
+{
+public:
+    SampleWidgets00 widget;
+
+    // The Ui is private, and the Go button is not in the public interface,
+    // so we"cheat" and search for the contained widgets ourselves.
+    // NOT A RECOMMENDED PRACTICE!!
+    // This makes for potential maintenance pain in future, if the widget
+    // internals are modified.
+    QToolButton* goButton()
+    {
+        auto gobutton = widget.findChild<QToolButton*>();
+        // Will give a run-time test failure if SampleWidgets00 no longer has
+        // a QToolButton
+        // But won't detect a change to use a QToolButton for a totally different
+        // purpose
+        REQUIRE(gobutton != nullptr);
+        return gobutton;
+    }
+};
+
+TEST_CASE_METHOD(
+    SampleWidgets00Fixture, "SampleWidgets00 gobutton is initially unchecked")
+{
+    auto gobutton = goButton();
+    CHECK(!gobutton->isChecked());
+}
