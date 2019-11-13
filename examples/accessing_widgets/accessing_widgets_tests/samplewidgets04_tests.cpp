3566efd2caffd61c1f52e4c48bb674053ea6af9a
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-13 11:17:19 +0000

    t SampleWidgets04 now uses friendship without inheritance

--- examples/accessing_widgets/accessing_widgets_tests/samplewidgets04_tests.cpp
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets04_tests.cpp
@@ -4,14 +4,23 @@
 
 #include <QToolButton>
 
-// By inheriting SampleWidgets04, we can expose access to its protected interface
-class SampleWidgets04Fixture : public SampleWidgets04
+// No iheritance - we can access the private interface of SampleWidgets04
+// due to friendship.
+class SampleWidgets04Fixture
 {
 public:
-    // Bump visibility from private to public, for testing (via friendship)
-    // We need to do this because friendship isn't inherited, and any tests
-    // that use this fixture do so by Catch generating code that inherits it.
-    using SampleWidgets04::goButton;
+    SampleWidgets04 widget;
+
+    // Wrapper to bump visibility from private to public, for testing (via
+    // friendship). We need to do this because friendship isn't inherited, and
+    // any tests that use this fixture do so by Catch generating code that
+    // inherits it.
+    QToolButton* goButton()
+    {
+        // We are calling a private method, but are allowed to do so here
+        // because this test fixture is a friend of SampleWidgets04
+        return widget.goButton();
+    }
 };
 
 TEST_CASE_METHOD(
