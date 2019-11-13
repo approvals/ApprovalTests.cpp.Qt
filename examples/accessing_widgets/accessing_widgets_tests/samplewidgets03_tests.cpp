a49c0d36fe9cf86ce26c8cfd5f98fe651645dbb1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-13 11:34:02 +0000

    t Add some commentary on pros and cons of SampleWidgets03Fixture

--- examples/accessing_widgets/accessing_widgets_tests/samplewidgets03_tests.cpp
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets03_tests.cpp
@@ -4,6 +4,18 @@
 
 #include <QToolButton>
 
+// OPINION: This offers the best combination of being:
+//  1.  least intrusive on the interface for users of SampleWidgets03
+//  2.  Least maintenance effort here... no need to update the signature of
+//      goButton() here if it changes in the base class.
+//  3.  Easy to add 'using' statements for any new private methods added
+//      to the class being tested.
+//
+// One possible confusion, though, is that it is not obvious to those reading
+// the TEST_CASE_METHOD tests below that it is generating a class that
+// inherits SampleWidgets03. It might have been clearer to have a
+// SampleWidgets03 instance visible for calling.
+
 // By inheriting SampleWidgets03, we can expose access to its protected interface
 class SampleWidgets03Fixture : public SampleWidgets03
 {
