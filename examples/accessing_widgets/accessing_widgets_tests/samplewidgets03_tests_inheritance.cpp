16ba91eeacdfd5c2e7cd4ae98fc1abe5e5aa90a1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2019-11-14 12:05:24 +0000

    t Rename a test, for consinstency with other file testing same class

--- examples/accessing_widgets/accessing_widgets_tests/samplewidgets03_tests_inheritance.cpp
+++ examples/accessing_widgets/accessing_widgets_tests/samplewidgets03_tests_inheritance.cpp
@@ -26,7 +26,7 @@ public:
     using SampleWidgets03::goButton;
 };
 
-TEST_CASE_METHOD(SampleWidgets03FixtureInheritance, "SampleWidgets03 gobutton is initially unchecked")
+TEST_CASE_METHOD(SampleWidgets03FixtureInheritance, "SampleWidgets03 gobutton is initially unchecked - v1")
 {
     auto gobutton = goButton();
     CHECK(!gobutton->isChecked());
