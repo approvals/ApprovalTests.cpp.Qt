f37dde21f902c34027e66d0389b58c93a6f379d4
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-07 21:56:07 +0100

    r Add simulated_single_header_file

--- build/scripts_qt/qt_project_details.py
+++ build/scripts_qt/qt_project_details.py
@@ -5,6 +5,7 @@ def qt_project_details() -> ProjectDetails:
     details = ProjectDetails(
         github_project_name='ApprovalTests.cpp.Qt',
         library_folder_name='ApprovalTestsQt',
+        simulated_single_header_file='ApprovalTestsQt.hpp',
         version_header='ApprovalTestsQtVersion.h',
         macro_prefix='APPROVAL_TESTS_QT',
         conan_directory_name='approvaltests.cpp.qt',
