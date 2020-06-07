45557cf5beb34548e0421afd62e18fadbd9718a7
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-07 09:32:13 +0100

    F Add simulated single header (#5)
    This enables building from clone or fork of the project.

---
+++ ApprovalTestsQt/ApprovalTestsQt.hpp
@@ -0,0 +1,15 @@
+#ifndef APPROVAL_TESTS_CPP_QT_APPROVALS_HPP
+#define APPROVAL_TESTS_CPP_QT_APPROVALS_HPP
+
+// This file is machine-generated. Do not edit.
+
+#include "ApprovalTestsQt/integrations/LoadApprovals.h"
+#include "ApprovalTestsQt/writers/QTableViewWriter.h"
+#include "ApprovalTestsQt/writers/QImageApprovalWriter.h"
+#include "ApprovalTestsQt/ApprovalTestsQtVersion.h"
+#include "ApprovalTestsQt/ApprovalsQt.h"
+#include "ApprovalTestsQt/comparators/QImageApprovalComparator.h"
+#include "ApprovalTestsQt/integrations/catch/Catch2QtApprovals.h"
+#include "ApprovalTestsQt/integrations/catch/Catch2QtStringMaker.h"
+
+#endif // APPROVAL_TESTS_CPP_QT_APPROVALS_HPP
