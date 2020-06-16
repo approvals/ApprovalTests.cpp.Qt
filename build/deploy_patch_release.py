45e21213d5e44822ef1782dbf1d534a8c30d9413
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-16 23:00:14 +0100

    e   Copy in the remaining top-level release scripts from main project
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ build/deploy_patch_release.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+import scripts_qt.include_approvals_path # Note This is used!
+
+from scripts.prepare_release import build
+from scripts.project_details import ProjectDetails
+
+if __name__ == '__main__':
+    build(lambda v: v.update_patch(), deploy = True, project_details=ProjectDetails())
---
+++ build/restart_deploy_from_conan.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import scripts_qt.include_approvals_path # Note This is used!
+
+from scripts.deploy_release import DeployRelease
+from scripts.prepare_release import set_working_directory_and_load_current_version
+from scripts.project_details import ProjectDetails
+from scripts.release_details import ReleaseDetails
+
+if __name__ == '__main__':
+    project_details = ProjectDetails()
+    old_version = set_working_directory_and_load_current_version(project_details.library_folder_name)
+    new_version = old_version.clone()
+    details = ReleaseDetails(old_version, new_version, True, project_details)
+    deploy_release = DeployRelease(details)
+    deploy_release.push_everything_live(start_at_conan=True)
