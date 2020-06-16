867697d31834f02f3dbcca67fde9d4d899a1af49
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-16 15:23:44 +0100

    t   We now get the correct text for Conan branch name
    When this project is eventually added to Conan

--- build/tests/test_conan_release.py
+++ build/tests/test_conan_release.py
@@ -2,9 +2,10 @@ import unittest
 
 from scripts.conan_release import PrepareConanRelease
 from scripts.version import Version
+from scripts_qt.qt_project_details import qt_project_details
 
 
 class TestConanRelease(unittest.TestCase):
     def test_get_new_branch_name(self) -> None:
-        self.assertEqual('approvaltests.cpp.1.2.3', PrepareConanRelease.get_new_branch_name(Version(1, 2, 3)))
-
+        self.assertEqual('approvaltests.cpp.qt.1.2.3',
+                         PrepareConanRelease.get_new_branch_name(qt_project_details(), Version(1, 2, 3)))
