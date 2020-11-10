ef988b11303e5ff7eb7dd21c76282c6d5f00430e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-11-10 10:26:07 +0000

    t   Add comments showing what failure looks like

--- examples/vanilla_qt_tests/TestQString.cpp
+++ examples/vanilla_qt_tests/TestQString.cpp
@@ -12,8 +12,20 @@ private slots:
 void TestQString::toUpper()
 {
     QString str = "Hello";
+
     QVERIFY(str.toUpper() == "HELLO");
-    QCOMPARE(str.toUpper(), QString("HELLO")); // writes out content on failure
+    /* Failure would look like:
+FAIL!  : TestQString::toUpper() 'str.toUpper() == "HELLzO"' returned FALSE. ()
+Loc: [.../ApprovalTests.cpp.Qt/examples/vanilla_qt_tests/TestQString.cpp(15)]
+    */
+
+    QCOMPARE(str.toUpper(), QString("HELLO"));
+    /* Failure would look like:
+FAIL!  : TestQString::toUpper() Compared values are not the same
+Actual   (str.toUpper())    : "HELLO"
+Expected (QString("HELqLO")): "HELqLO"
+Loc: [.../ApprovalTests.cpp.Qt/examples/vanilla_qt_tests/TestQString.cpp(16)]
+    */
 }
 
 QTEST_MAIN(TestQString)
