fe3628f719cde875f9b704ac75e63c603b3fee4d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-11-10 10:53:18 +0000

    t   Add some tests to show what failure looks like

--- examples/vanilla_catch_qt_tests/TestNumbersInCatch2.cpp
+++ examples/vanilla_catch_qt_tests/TestNumbersInCatch2.cpp
@@ -5,4 +5,8 @@
 TEST_CASE("LessThan")
 {
     CHECK(12 < 42);
+    /* Failure would look like:
+.../ApprovalTests.cpp.Qt/examples/vanilla_catch_qt_tests/TestNumbersInCatch2.cpp:7: FAILED:
+CHECK( 52 < 42 )
+    */
 }
--- examples/vanilla_qt_tests/TestNumbers/TestNumbers.cpp
+++ examples/vanilla_qt_tests/TestNumbers/TestNumbers.cpp
@@ -10,6 +10,10 @@ private slots:
 void TestNumbers::lessThan()
 {
     QVERIFY(12 < 42);
+    /* Failure would look like:
+FAIL!  : TestNumbers::lessThan() '52 < 42' returned FALSE. ()
+Loc: [.../ApprovalTests.cpp.Qt/examples/vanilla_qt_tests/TestNumbers/TestNumbers.cpp(12)]
+    */
 }
 
 QTEST_MAIN(TestNumbers)
