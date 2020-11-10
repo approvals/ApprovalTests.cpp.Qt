72e7fd5392d875b6621e8795eae62c8c82e96ca1
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-11-10 12:25:50 +0000

    t   Temporary commit to remove comments

--- examples/vanilla_catch_qt_tests/TestQStringInCatch2.cpp
+++ examples/vanilla_catch_qt_tests/TestQStringInCatch2.cpp
@@ -6,10 +6,4 @@ TEST_CASE("String::toUpper")
 {
     QString str = "Hello";
     CHECK(str.toUpper() == "HELLO");
-    /* Failure would look like:
-.../ApprovalTests.cpp.Qt/examples/vanilla_catch_qt_tests/TestQStringInCatch2.cpp:8: FAILED:
-  CHECK( str.toUpper() == "HExLLO" )
-with expansion:
-  { {?}, {?}, {?}, {?}, {?} } == "HExLLO"
-    */
 }
